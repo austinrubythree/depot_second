08d5ad34a51ec9a9b05a1ca59bd4e03428700d80
austinrubythree <chin@therubythree.com>
2019-05-23 11:34:56 -0700

    chapter 9

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -46,6 +46,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
+  test "can't delete product in cart" do
+    assert_difference('Product.count', -1) do
+      delete product_url(@product)
+    end
+    assert_redirected_to products_url
+  end
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
@@ -70,4 +77,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
         end
       end 
     end
+  
+  
+
 end
